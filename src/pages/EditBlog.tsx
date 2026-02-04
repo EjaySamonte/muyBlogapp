aa54acb3f522c7ea620f6bd5839f63bc155907d2
EjaySamonte <ericjayangelo@gmail.com>
2026-02-04 15:04:53 +0800

    edit

--- src/pages/EditBlog.tsx
+++ src/pages/EditBlog.tsx
@@ -2,14 +2,17 @@ import React, { useEffect, useState} from "react";
 import { supabase } from "../supabase/client";
 import { useNavigate, useParams } from "react-router-dom";
 import './EditBlog.css';
+import { upload } from "@testing-library/user-event/dist/upload";
 
 export default function EditBlog() {
     const { id } = useParams();
     console.log("Editlog ID:", id)
     const navigate = useNavigate();
-    const [, setError] = useState<string | null>(null);
+    const [ , setError] = useState<string | null>(null);
     const [title, setTitle] = useState("");
     const [content, setContent] = useState ("");
+    const [imageFile, setImageFile] = useState<File | null>(null);
+    
 
     useEffect(() => {
         const fetchBlog = async() => {
@@ -30,9 +33,24 @@ export default function EditBlog() {
     const handleUpdate = async (e: React.FormEvent) => {
         e.preventDefault();
         
+        let imageURL: string | null = null;
+
+        if(imageFile) {
+            const fileName = `${Date.now()}-${imageFile.name}`;
+            const {error: uploadError} = await supabase.storage.from("blog-images").upload(fileName, imageFile);
+
+            if (uploadError) {
+                console.error("Upload error", uploadError.message)
+                setError(`Image upload failed: ${uploadError.message}`)
+            }
+
+            const {data} = supabase.storage.from("blog-images").getPublicUrl(fileName);
+            imageURL = data.publicUrl;
+        }
+        
         const { error } = await supabase
             .from("blogs")
-            .update({ title, content })
+            .update({ title, content, ...(imageURL ? {image_url: imageURL} : {}) })
             .eq("id", id);
 
         if (error) {
@@ -46,6 +64,7 @@ export default function EditBlog() {
         const handleCancel = () => {
             navigate("/my-blogs")
         }
+        
 
     return (
         <div className="editBlogHeader">
@@ -56,6 +75,7 @@ export default function EditBlog() {
                     <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}
                     placeholder="Blog Title" />
                     <textarea value={content} onChange={(e) => setContent(e.target.value)} placeholder="Type here." />
+                    <input type="file" accept="image/*" onChange={(e) => setImageFile(e.target.files?.[0] || null)}></input>
                     <button type="submit">Save Changes</button>
                     <button type="button" onClick={handleCancel} className="cancelButton">Cancel</button>
                 </form>
